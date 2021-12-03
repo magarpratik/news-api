d316b6f14a6a7c0ddfbf37ae874bfddd576d5590
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2021-12-03 01:44:51 +0000

    cchanged the json file to the correct one for displaying endpoints' descriptions at GET /api

--- routers/api-router.js
+++ routers/api-router.js
@@ -2,7 +2,7 @@ const apiRouter = require("express").Router();
 const topicRouter = require("./topic-router");
 const articleRouter = require("./article-router");
 const commentRouter = require("./comment-router");
-const endpoints = require("../package.json");
+const endpoints = require("../endpoints.json");
 
 apiRouter.use("/topics", topicRouter);
 apiRouter.use("/articles", articleRouter);
