9d2eecaecfdf8fbfd3fe77f99503bcc3254d6a70
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2021-12-03 09:58:16 +0000

    added handlePathErrors() in apiRouter to handle incorrect urls

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,6 +3,7 @@ const topicRouter = require("./topic-router");
 const articleRouter = require("./article-router");
 const commentRouter = require("./comment-router");
 const endpoints = require("../endpoints.json");
+const { handlePathErrors } = require("../errors/errors");
 
 apiRouter.use("/topics", topicRouter);
 apiRouter.use("/articles", articleRouter);
@@ -11,5 +12,7 @@ apiRouter.use("/comments", commentRouter);
 apiRouter.route("/").get((req, res) => {
   res.status(200).send(endpoints);
 });
+
+apiRouter.all("/*", handlePathErrors);
 
 module.exports = apiRouter;
